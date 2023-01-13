87ef86776e7b12b2a888dd4114a7f44b942bc0bd
Lukasz Jasinski <jasinskilukasz231@gmail.com>
2023-01-13 10:19:16 +0100

    ButtonClass changes. Main file - created first button object. Everything works.

--- ButtonClass.cs
+++ ButtonClass.cs
@@ -1,32 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 public class ButtonClass
 {
-    int x, y, width, height;
+    int x, y, w, h;
     string t;
     Button button = new Button();
 
-    button.Left=x;
-    button.Top=y;
-    button.Width=widht;
-    button.Height=height;
-    button.Text=t;
-    Controls.Add(button);
-
-    ButtonClass(int pos_x, int pos_y, int width, int height, string text) 
+    public Button CreateButton()
     {
-        x=pos_x;
-        y=pos_y;
-        width=width;
-        height=height;
+        button.Left = x;
+        button.Top = y;
+        button.Width = w;
+        button.Height = h;
+        button.Text = t;
+        button.Visible = true;
+        return button;
+    }
+    public ButtonClass(int pos_x, int pos_y, int width, int height, string text) 
+    {
+        x = pos_x - width / 2;
+        y = pos_y - height / 2;
+        w=width;
+        h=height;
         t=text;
     }
 }
--- Main.cs
+++ Main.cs
@@ -12,9 +12,19 @@ namespace BookStoreApp
 {
     public partial class WindowFrame : Form
     {
+        const int win_x = 1262, win_y = 673; //size of the window
         public WindowFrame()
         {
             InitializeComponent();
+            CreateStartingScreen();
+            
+        }
+
+        private void CreateStartingScreen()
+        {
+            ButtonClass registerButton = new ButtonClass(win_x / 2, win_y / 2, 150, 50, "Register");
+            Controls.Add(registerButton.CreateButton());
+            
         }
     }
 }
