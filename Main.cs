cc16599b689313a06975b1fea40d67f7308eab0e
Lukasz Jasinski <jasinskilukasz231@gmail.com>
2023-01-14 18:03:43 +0100

    Creating starting and render screens. Optimalizing the rest. Starting screen wont hide

--- Main.cs
+++ Main.cs
@@ -13,59 +13,152 @@ namespace BookStoreApp
     public partial class WindowFrame : Form
     {
         const int win_x = 1262, win_y = 673; //size of the window
+        int whatToRender = 1; //variable that tells which screen should be rendered
+        //0->nothing
+        //1->init all screens
+        //2->starting screen
+        //3->register screen
+
+        StartingScreen startingScreen = new StartingScreen();
+        RegisterScreen registerScreen = new RegisterScreen();
+
+        Timer appTimer = new Timer();
         public WindowFrame()
         {
+            InitResources();
             InitializeComponent();
-            CreateStartingScreen();
+            InitStartingScreen();
+            ShowStartingScreen();
         }
-
-        private void CreateStartingScreen()
+        private void InitResources()
         {
-            ButtonClass StartingScreenRegisterButton = new ButtonClass(win_x / 2, win_y / 2 + 250, 150, 50, "Register");
-            Controls.Add(StartingScreenRegisterButton.GetObject());
-            StartingScreenRegisterButton.GetObject().Click += new EventHandler(StartingScreenRegisterButtonClick);
-
-            ButtonClass StartingScreenStafLoginButton = new ButtonClass(win_x / 2, win_y / 2 + 310, 150, 50, "Staf Login");
-            Controls.Add(StartingScreenStafLoginButton.GetObject());
-            StartingScreenStafLoginButton.GetObject().Click += new EventHandler(StartingScreenStafLoginButtonClick);
-
-            ButtonClass StartingScreenLoginButton = new ButtonClass(win_x / 2, win_y / 2 + 170, 150, 50, "Login");
-            Controls.Add(StartingScreenLoginButton.GetObject());
-            StartingScreenLoginButton.GetObject().Click += new EventHandler(StartingScreenLoginButtonClick);
-
-            TextBoxClass StartingScreenTextBoxLogin = new TextBoxClass(win_x / 2, win_y / 2, 200);
-            Controls.Add(StartingScreenTextBoxLogin.GetObject());
-            StartingScreenTextBoxLogin.GetObject().Font = UtilitiesClass.arial12Regular;
-
-            TextBoxClass StartingScreenTextBoxPassword = new TextBoxClass(win_x / 2, win_y / 2 + 70, 200);
-            Controls.Add(StartingScreenTextBoxPassword.GetObject());
-            StartingScreenTextBoxPassword.GetObject().Font = UtilitiesClass.arial12Regular;
-
-            LabelClass StartingScreenLoginLabel = new LabelClass(win_x / 2 - 160, win_y / 2 + 30, "Login", 150, 50);
-            Controls.Add(StartingScreenLoginLabel.GetObject());
-            StartingScreenLoginLabel.GetObject().Font = UtilitiesClass.arial12Bold;
-
-            LabelClass StartingScreenPasswordLabel = new LabelClass(win_x / 2 - 160, win_y / 2 + 100, "Password", 150, 50);
-            Controls.Add(StartingScreenPasswordLabel.GetObject());
-            StartingScreenPasswordLabel.GetObject().Font = UtilitiesClass.arial12Bold;
-
-            LabelClass StartingScreenWrongDataLabel = new LabelClass(win_x / 2 + 20, win_y / 2 + 120, "Wrong login or password", 150, 30);
-            Controls.Add(StartingScreenWrongDataLabel.GetObject());
-            StartingScreenWrongDataLabel.GetObject().ForeColor = Color.Red;
-            StartingScreenWrongDataLabel.GetObject().Visible = false;
-
+            appTimer.Enabled = true;
+            appTimer.Start();
+            appTimer.Tick += new EventHandler(AppRunning);
         }
-        private void StartingScreenRegisterButtonClick(object sender, EventArgs e)
+        private void AppRunning(object sender, EventArgs e)
         {
+            switch (whatToRender)
+            {
+                case 1: //inits
+                    InitStartingScreen();
+                    InitRegisterScreen();
+                    //ShowStartingScreen();
+                    //HideRegisterScreen();
+                    whatToRender = 0;
+                    break;
+                case 2: //show starting screen
+                    //ShowStartingScreen();
+                    //HideRegisterScreen();
+                    whatToRender = 0;
+                    break;
+                case 3: //show register screen
+                    //ShowRegisterScreen();
+                    //HideStartingScreen();
+                    whatToRender = 0;
+                    break;
+                default: //0->nothing
 
+                    if (startingScreen.StartingScreenRegisterButtonPressed==true)
+                    {
+                        whatToRender = 3;
+                        startingScreen.StartingScreenRegisterButtonPressed = false; 
+                    }
+                    break;
+
+            }
         }
-        private void StartingScreenLoginButtonClick(object sender, EventArgs e)
+        private void InitStartingScreen()
         {
-
+            startingScreen.CreateStartingScreen(win_x, win_y);
+            Controls.Add(startingScreen.StartingScreenRegisterButton.GetObject());
+            Controls.Add(startingScreen.StartingScreenStafLoginButton.GetObject());
+            Controls.Add(startingScreen.StartingScreenLoginButton.GetObject());
+            Controls.Add(startingScreen.StartingScreenTextBoxLogin.GetObject());
+            Controls.Add(startingScreen.StartingScreenTextBoxPassword.GetObject());
+            Controls.Add(startingScreen.StartingScreenLoginLabel.GetObject());
+            Controls.Add(startingScreen.StartingScreenPasswordLabel.GetObject());
+            Controls.Add(startingScreen.StartingScreenWrongDataLabel.GetObject());
         }
-        private void StartingScreenStafLoginButtonClick(object sender, EventArgs e)
+        private void HideStartingScreen()
         {
-
+            startingScreen.StartingScreenRegisterButton.GetObject().Visible = false;
+            startingScreen.StartingScreenStafLoginButton.GetObject().Visible = false;
+            startingScreen.StartingScreenLoginButton.GetObject().Visible = false;
+            startingScreen.StartingScreenTextBoxLogin.GetObject().Visible = false;
+            startingScreen.StartingScreenTextBoxPassword.GetObject().Visible = false;
+            startingScreen.StartingScreenLoginLabel.GetObject().Visible = false;
+            startingScreen.StartingScreenPasswordLabel.GetObject().Visible = false;
+            startingScreen.StartingScreenWrongDataLabel.GetObject().Visible = false;
+        }
+        private void ShowStartingScreen()
+        {
+            startingScreen.StartingScreenRegisterButton.GetObject().Visible = true;
+            startingScreen.StartingScreenStafLoginButton.GetObject().Visible = true;
+            startingScreen.StartingScreenLoginButton.GetObject().Visible = true;
+            startingScreen.StartingScreenTextBoxLogin.GetObject().Visible = true;
+            startingScreen.StartingScreenTextBoxPassword.GetObject().Visible = true;
+            startingScreen.StartingScreenLoginLabel.GetObject().Visible = true;
+            startingScreen.StartingScreenPasswordLabel.GetObject().Visible = true;
+            startingScreen.StartingScreenWrongDataLabel.GetObject().Visible = true;
+        }
+        private void InitRegisterScreen()
+        {
+            registerScreen.CreateRegisterScreen(win_x,win_y);
+            Controls.Add(registerScreen.RegisterScreenRegisterButton.GetObject());
+            Controls.Add(registerScreen.RegisterScreenNameTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenLastNameTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenEmailTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenPhoneTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenAddressTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenLoginTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenPasswordTextBox.GetObject());
+            Controls.Add(registerScreen.RegisterScreenNameLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenLastNameLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenEmailLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenPhoneLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenAddressLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenSexLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenLoginLabel.GetObject());
+            Controls.Add(registerScreen.RegisterScreenPasswordLabel.GetObject());
+        }
+        private void ShowRegisterScreen()
+        {
+            registerScreen.RegisterScreenRegisterButton.GetObject().Visible = true;
+            registerScreen.RegisterScreenNameTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenLastNameTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenEmailTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenPhoneTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenAddressTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenLoginTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenPasswordTextBox.GetObject().Visible = true;
+            registerScreen.RegisterScreenNameLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenLastNameLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenEmailLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenPhoneLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenAddressLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenSexLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenLoginLabel.GetObject().Visible = true;
+            registerScreen.RegisterScreenPasswordLabel.GetObject().Visible = true;
+        }
+        private void HideRegisterScreen()
+        {
+            registerScreen.RegisterScreenRegisterButton.GetObject().Visible = false;
+            registerScreen.RegisterScreenNameTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenLastNameTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenEmailTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenPhoneTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenAddressTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenLoginTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenPasswordTextBox.GetObject().Visible = false;
+            registerScreen.RegisterScreenNameLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenLastNameLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenEmailLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenPhoneLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenAddressLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenSexLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenLoginLabel.GetObject().Visible = false;
+            registerScreen.RegisterScreenPasswordLabel.GetObject().Visible = false;
         }
     }
 }
